853fe82eb0599282008c964f490d0830dd5f0e9e
Davinia pd <delgadavi@gmail.com>
2024-08-12 19:34:46 +0000

    hecho el funcionamiento de añadir items y que se muestren en el html y añadido el tag de footer sin contenido dinamico aun

--- src/js/component/nottodolist.jsx
+++ src/js/component/nottodolist.jsx
@@ -1,18 +1,50 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 
 const NotToDoList = () => {
 
-    const [listItems, setListItems] = useState (["Your list is empty, I can't believe you have no regrets."]);
+    // const [listItems, setListItems] = useState(["Your list is empty, I can't believe you have no regrets."]);
+    const [listItems, setListItems] = useState([]);
+    const [newItem, setNewItem] = useState('');
 
-    
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            setListItems(listItems.concat([newItem]));
+            setNewItem('');
+        }
+    };
+
+
     return (
         <>
 
-        <h1 className="text-center m-5">Not To Do List</h1>
-        <input className="form-control form-control-lg w-50 mx-auto" type="text" placeholder="What would you regret doing?" aria-label="Not to do item" />
-        <ul className="w-50 mx-auto my-5">
-            {listItems.map ( (listItem, index) => <h4 key={index}>{listItem}</h4>)}
-        </ul>
+            <h1 className="text-center m-5">Not To Do List</h1>
+            <input
+                className="form-control form-control-lg w-50 mx-auto"
+                type="text"
+                placeholder="What would you regret doing?"
+                aria-label="Not to do item"
+                value={newItem}
+                onChange={(e) => setNewItem(e.target.value)}
+                onKeyUp={handleKeyPress}
+            />
+            <ul className="w-50 mx-auto my-5 bg-warning bg-opacity-50 p-5">
+                {listItems.length === 0 && (
+                    <h4 className="text-center text-danger">Your list is empty, I can't believe you have no regrets.</h4>
+                )}
+                {listItems.map((listItem, index) => (
+                    <h4 key={index}>{listItem}</h4>
+                ))}
+                <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                    <div class="col-md-4 d-flex align-items-center">
+                        <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+                            <svg class="bi" width="30" height="24"></svg>
+                        </a>
+                        <span class="mb-3 mb-md-0 text-body-secondary opacity-75">count</span>
+                    </div>
+                </footer>
+            </ul>
+
+
 
         </>
     )
